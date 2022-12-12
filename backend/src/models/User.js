1b372f31f0032580ada314ac5856c1cde8e6b2ee
marlleyck <marlleyck@hotmail.com>
2022-12-12 19:24:19 -0300

    chore: created models

---
+++ backend/src/models/User.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize')
+const db = require('../database')
+
+const User = db.define('User', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    password: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    exercises: {
+        type: DataTypes.JSON,
+        allowNull: false
+    }
+})
+
+module.exports = User
