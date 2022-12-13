e3edbd00928a8f58c02e78d337b1402553f36395
marlleyck <marlleyck@hotmail.com>
2022-12-13 11:34:00 -0300

    feat: add route register

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,12 +1,14 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import { Home } from "./pages/Home"
+import { Home } from './pages/Home'
+import { Register } from './pages/Register'
 
 const AppRoutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={ <Home /> } />
+                <Route path="/" element={<Home />} />
+                <Route path="/register" element={<Register />} />
             </Routes>
         </BrowserRouter>
     )
