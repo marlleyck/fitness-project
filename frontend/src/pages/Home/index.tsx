c6bcda269b842f36b76a4f39a415e5debd9154be
marlleyck <marlleyck@hotmail.com>
2022-12-13 00:05:32 -0300

    style: changed title home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -4,7 +4,7 @@ export const Home = () => {
     return (
         <div className="w-screen h-screen bg-black flex items-center justify-start flex-col">
             <header>
-                <h1 className="text-white font-Open_Sans text-6xl">Gen Fitness</h1>
+                <h1 className="text-white font-Open_Sans text-6xl font-thin">Gym Fitness</h1>
             </header>
             
 
