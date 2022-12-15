5cd9433da2df7c0055ffebd41053d4ae958cf4d4
marlleyck <marlleyck@hotmail.com>
2022-12-15 12:22:23 -0300

    style: add new exercise button on profile page

--- frontend/src/components/ProfileMain/index.tsx
+++ frontend/src/components/ProfileMain/index.tsx
@@ -1,18 +1,16 @@
 export const ProfileMain = () => {
     return (
         <div className="w-11/12 h-2/4 bg-black-cyan shadow-4xl flex items-center justify-start flex-col">
-            <header className="w-full h-full pb-4 border-solid border-black">
-                <div className="w-full h-full flex items-center justify-center flex-col p-4">
-                    <div className="w-full flex items-center flex-col">
-                        <button
-                            className="w-40 p-3 rounded-xl bg-green text-white hover:brightness-75 duration-500"
-                            type="button"
-                        >
-                            Novo treino
-                        </button>
-                    </div>
+            <div className="w-full h-full flex items-center justify-center flex-col p-4 relative">
+                <div className="flex items-center flex-col absolute top-0 m-3">
+                    <button
+                        className="w-40 p-3 rounded-xl bg-green text-white hover:brightness-75 duration-500"
+                        type="button"
+                    >
+                        Novo treino
+                    </button>
                 </div>
-            </header>
+            </div>
         </div>
     );
 };
