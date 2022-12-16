c5bd5456f991bea05bf50d208b012e18e1d25268
marlleyck <marlleyck@hotmail.com>
2022-12-16 11:50:49 -0300

    chore: add new color on tailwind config

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
                 'black-cyan': '#1F2730',
                 purple: '#6B54B7',
                 green: '#269A90',
+                'bg-modal': 'rba(0,0,0,0.4)',
             },
             boxShadow: {
                 '4xl': '0 35px 60px 18px rgba(0, 0, 0, 0.3)',
