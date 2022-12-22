0d37317ef95cbbfef6b255f9dfd554c6c4f885ab
marlleyck <marlleyck@hotmail.com>
2022-12-22 19:57:38 -0300

    chore: add media query to home page

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -7,6 +7,10 @@ module.exports = {
             Montserrat: ['"Montserrat"'],
         },
         extend: {
+            screens: {
+                md: { max: '890px' },
+                xs: { max: '520px' },
+            },
             colors: {
                 'black-cyan': '#1F2730',
                 purple: '#6B54B7',
